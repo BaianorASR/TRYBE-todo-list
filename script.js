c784ac28f5ff0fdc0890e16a8a2385dce5142d59
BaianorASR <reis.adson2@outlook.com>
2021-12-10 22:50:01 -0300

    9 - passed

--- script.js
+++ script.js
@@ -17,7 +17,8 @@ function adicionaTexto(elem) {
 
 function escutador(elem) {
   elem.addEventListener('click', seleciona);
-};
+  elem.addEventListener('dblclick', completo);
+}
 
 // Função que cria elementos
 function criaElementos(paramPai, paramFilho, tipo) {
@@ -27,22 +28,22 @@ function criaElementos(paramPai, paramFilho, tipo) {
   if (tipo !== undefined) {
     tag(filho, tipo);
   }
-  adicionaTexto(filho);
-  escutador(filho);
+  if (paramFilho === 'li') {
+    adicionaTexto(filho);
+    escutador(filho);
+  }
 }
 
-/*
-  5 =>
-*/
+//  5 =>
+
 // Chama a função que cria a li e atualiza para remover a class selecionado de todos li
 button.addEventListener('click', () => {
   criaElementos('#lista-tarefas', 'li', ['class', 'tarefas']);
   //atualizaElementos();
 });
 
-/*
-  7 - 8 =>
-*/
+//  7 - 8 =>
+
 //remove selecionado das li
 function desseleciona() {
   for (let i = 0; i < tarefas.length; i += 1) {
@@ -50,9 +51,27 @@ function desseleciona() {
     tarefas[i].style.backgroundColor = '';
   }
 }
+
 // seleciona li
 function seleciona() {
   desseleciona();
   event.target.classList.add('selecionado');
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
+
+// 9 =>
+//  https://www.javascripttutorial.net/dom/css/check-if-an-element-contains-a-class/
+// coloca class completed apos duplo clique, e remove se já tiver
+// tambem coloca e remove o riscado na letra
+function completo() {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+    event.target.style.textDecoration = '';
+  } else {
+    event.target.classList.add('completed');
+    event.target.style.textDecoration = 'line-through';
+  }
+}
+
+// 10 =>
+
